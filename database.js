f8daa55e00d1b0852894219172738e5cca454289
Julia B <julia.bohlin@hyperisland.se>
2026-03-13 11:34:59 +0100

    export

--- database.js
+++ database.js
@@ -1,58 +1,48 @@
-
 const { MongoClient } = require('mongodb')
 const client = new MongoClient(process.env.MONGODB_URI)
 let collection;
 
-async function startDatabase() {
+async function getCollection() {
     if (!collection) {
         await client.connect()
         const db = client.db('music-library')
         collection = db.collection('playlist')
     }
+    return collection
+}
+
+async function startDatabase() {
+    await getCollection()
 }
 
 async function showAll() {
-    const all = await collection.find({}).toArray()
-    return all
+    const col = await getCollection()
+    return await col.find({}).toArray()
 }
 
 async function filterByArtist(artist) {
-    const song = await collection.find({
-        artist: artist
-    }).toArray()
-    return song
+    const col = await getCollection()
+    return await col.find({ artist }).toArray()
 }
 
 async function addSong(title, artist, album, year) {
-    const isExisting = await collection.findOne({
-        title: title,
-        artist: artist
-    })
+    const col = await getCollection()
+    const isExisting = await col.findOne({ title, artist })
     if (isExisting) {
         return { currentlyExisting: true }
-    } else {
-        await collection.insertOne({
-            title: title,
-            artist: artist,
-            album: album,
-            year: year
-        })
-        return { currentlyExisting: false }
     }
+    await col.insertOne({ title, artist, album, year })
+    return { currentlyExisting: false }
 }
 
 async function deleteSong(id) {
-    const isExisting = await collection.findOne({
-        _id: id
-    })
+    const col = await getCollection()
+    const isExisting = await col.findOne({ _id: id })
     if (!isExisting) {
         return { currentlyExisting: false }
-    } else {
-        await collection.deleteOne({
-            _id: id
-        })
-        return { currentlyExisting: true }
     }
+    await col.deleteOne({ _id: id })
+    return { currentlyExisting: true }
 }
 
 module.exports = {
